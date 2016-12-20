a2f8d5eed6d87ab700c62cd481787641de885701
Keith Chester <kchester@gmail.com>
2016-12-20 10:07:32 -0600

    fix: Handling arrays has been fixed
    Arrays were simply not being handled correctly. They detect as objects but
    I never checked specifically for them, so it would ugily list them out
    with the indexes as attributes. This would cause major issues in some
    applications, so I added logic to handle if both merge objects are arrays
    or if one or the other was an array.

--- index.js
+++ index.js
@@ -22,15 +22,35 @@ module.exports = function(){
 	
 	var iterateAndMerge = function(obj1, obj2){
 		var result = clone(obj1);
-		for(var attr in obj2){
-			if(!result[attr]){
-				result[attr] = clone(obj2[attr]);
-			} else if(typeof result[attr] == 'object' && typeof obj2[attr] == 'object'){
-				result[attr] = iterateAndMerge(obj1[attr], obj2[attr]);
-			} else if(onConflict){
-				result[attr] = onConflict(obj1[attr], obj2[attr], attr);
-			} else {
-				result[attr] = clone(obj2[attr]);
+		if(Array.isArray(obj1) && Array.isArray(obj2)){
+			obj2.forEach(function(item){
+				if(result.indexOf(item) == -1) result.push(item);
+			});
+		} else if(Array.isArray(obj1)){
+			//Switch result source - always do the object over the array
+			result = clone(obj2);
+			obj1.forEach(function(item){
+				//Does it exist? If so, iterate and merge first
+				if(result[item]) iterateAndMerge(result[item], item);
+				else result[item] = true;
+			});
+		} else if(Array.isArray(obj2)){
+			obj2.forEach(function(item){
+				//Does it exist? If so, iterate and merge first
+				if(result[item]) iterateAndMerge(result[item], item);
+				else result[item] = true;
+			});
+		} else {
+			for(var attr in obj2){
+				if(!result[attr]){
+					result[attr] = clone(obj2[attr]);
+				} else if(typeof result[attr] == 'object' && typeof obj2[attr] == 'object'){
+					result[attr] = iterateAndMerge(obj1[attr], obj2[attr]);
+				} else if(onConflict){
+					result[attr] = onConflict(obj1[attr], obj2[attr], attr);
+				} else {
+					result[attr] = clone(obj2[attr]);
+				}
 			}
 		}
 		
